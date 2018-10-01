91917fe732fdff783026b153b4b8fd9367951ce2
reneeb <info@perl-services.de>
2018-10-01 10:27:35 +0200

    changes for OTRS6

--- Custom/Kernel/Output/HTML/FilterContent/DashboardHideSidebar.pm
+++ Custom/Kernel/Output/HTML/FilterContent/DashboardHideSidebar.pm
@@ -1,6 +1,5 @@
 # --
-# Kernel/Output/HTML/FilterContent/DashboardHideSidebar.pm
-# Copyright (C) 2015-2016 Perl-Services.de, http://www.perl-services.de/
+# Copyright (C) 2015-2018 Perl-Services.de, http://www.perl-services.de/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
